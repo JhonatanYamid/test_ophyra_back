f09f1a432d82576cc2cdf82707178c0ba8fae690
Jhonatan <yamidjhonatan@gmail.com>
2022-07-19 13:04:39 -0500

    fix

--- Controller/Api/ProductsController.php
+++ Controller/Api/ProductsController.php
@@ -30,26 +30,22 @@ class ProductsController extends BaseController
 	{
 		$strErrorDesc = '';
 		try {
-			// $data = json_decode(file_get_contents('php://input'));
-			// $products = new Products();
-			// $file = null;
-			// if ($data->image->file != '' && $data->image->file != NULL) {
-			// 	$base64string = $data->image->file;
-			// 	$uploadpath   = 'images/';
-			// 	$parts        = explode(";base64,", $base64string);
-			// 	$imageparts   = explode("image/", @$parts[0]);
-			// 	$imagetype    = $imageparts[1];
-			// 	$imagebase64  = base64_decode($parts[1]);
-			// 	$file         = $uploadpath . uniqid() . '.' . $imagetype;
-			// 	file_put_contents($file, $imagebase64);
-			// }
-
-			// $action = $products->insert($data->title, $data->price, $data->description, $file);
-			// $response = json_encode($action);
 			$data = json_decode(file_get_contents('php://input'));
 			$products = new Products();
-			$productsList = $products->getAll($data->start, $data->limit);
-			$response = json_encode($productsList);
+			$file = null;
+			if ($data->image->file != '' && $data->image->file != NULL) {
+				$base64string = $data->image->file;
+				$uploadpath   = 'images/';
+				$parts        = explode(";base64,", $base64string);
+				$imageparts   = explode("image/", @$parts[0]);
+				$imagetype    = $imageparts[1];
+				$imagebase64  = base64_decode($parts[1]);
+				$file         = $uploadpath . uniqid() . '.' . $imagetype;
+				file_put_contents($file, $imagebase64);
+			}
+
+			$action = $products->insert($data->title, $data->price, $data->description, $file);
+			$response = json_encode($action);
 		} catch (Error $e) {
 			$strErrorDesc = $e->getMessage() . 'Algo salió mal!';
 			$strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
