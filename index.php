ef01a87986d9e333b3f348ba73e6e23ad98c3c0c
Jhonatan <yamidjhonatan@gmail.com>
2022-07-19 12:28:29 -0500

    fix

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ require __DIR__ . "/config/bootstrap.php";
  
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
-var_dump($uri[2]);
-if ((isset($uri[2]) && $uri[2] != 'products') || !isset($uri[3])) {
-    header("HTTP/1.1 404 Not Found");
-    exit();
-}
+
+// if ((isset($uri[2]) && $uri[2] != 'products') || !isset($uri[3])) {
+//     header("HTTP/1.1 404 Not Found");
+//     exit();
+// }
 require PROJECT_ROOT_PATH . "/Controller/Api/ProductsController.php";
 $objFeedController = new ProductsController();
 $strMethodName = $uri[3] . 'Action';
