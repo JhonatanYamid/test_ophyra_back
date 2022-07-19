82cdfa747459eadd86cb0caf0f8a123a77b94e8b
Jhonatan <yamidjhonatan@gmail.com>
2022-07-19 12:24:05 -0500

    fix

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require __DIR__ . "/config/bootstrap.php";
  
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
+var_dump($uri[2]);
 if ((isset($uri[2]) && $uri[2] != 'products') || !isset($uri[3])) {
     header("HTTP/1.1 404 Not Found");
     exit();
